6f4168c9c8e2d46a08df1467fe365cedda4487e9
Suman Kumar <skumar21@vmware.com>
2025-03-24 12:31:49 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from './Header'
+import Food from './Food'
+import Footer from './Footer'
+
+import './App.css'
+
+function App() {
+  return(
+    <>
+      <Header/>
+      <Food/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App
