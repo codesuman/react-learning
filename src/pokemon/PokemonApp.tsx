cb833aa9c798dfc0f42cc8777b83cb3c50eb2445
Suman Kumar <skumar21@vmware.com>
2025-05-27 11:12:27 +0530

    Adding debounce implementation for Search

--- src/pokemon/PokemonApp.tsx
+++ src/pokemon/PokemonApp.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { PokemonActionType, usePokemonContext, type PokemonContextType, type State } from "./PokemonStore";
 
 const PokemonList = () => {
@@ -23,11 +24,22 @@ const PokemonList = () => {
 
 const PokemonSearch = () => {
     const {state, dispatch}: PokemonContextType = usePokemonContext();
+    const [searchText, setSearchText] = useState(state.search || '');
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            if(searchText !== state.search)
+                dispatch({type: PokemonActionType.SEARCH, payload: searchText});
+        }, 500); // ⏱ 500ms debounce
+
+        return () => clearTimeout(timer); // ❌ clear previous timeout
+    }, [searchText]);
 
     return (
         <input type="text" name="search" id="search" 
             placeholder="Enter search text..."
-            value={state.search} onChange={(event) => dispatch({type: PokemonActionType.SEARCH, payload: event.target.value})}/>
+            value={searchText} 
+            onChange={(event) => setSearchText(event.target.value)}/>
     );
 }
 
